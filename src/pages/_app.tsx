435dc73ea2a12be3ceeb5efe65ae4e83507dd2d4
Jørgen Johannesen <jorjo@stud.ntnu.no>
2023-12-07 14:08:26 +0100

    add graphql and code generator

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '~/styles/global.css'
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import type { AppProps } from 'next/app'
-import { IBM_Plex_Mono, Inter, PT_Serif } from 'next/font/google'
 import { lazy } from 'react'
 
 export interface SharedPageProps {
@@ -10,48 +10,26 @@ export interface SharedPageProps {
 }
 
 const PreviewProvider = lazy(() => import('~/components/PreviewProvider'))
-
-const mono = IBM_Plex_Mono({
-  variable: '--font-family-mono',
-  subsets: ['latin'],
-  weight: ['500', '700'],
-})
-
-const sans = Inter({
-  variable: '--font-family-sans',
-  subsets: ['latin'],
-  weight: ['500', '700', '800'],
-})
-
-const serif = PT_Serif({
-  variable: '--font-family-serif',
-  style: ['normal', 'italic'],
-  subsets: ['latin'],
-  weight: ['400', '700'],
-})
+const queryClient = new QueryClient()
 
 export default function App({
   Component,
   pageProps,
 }: AppProps<SharedPageProps>) {
   const { draftMode, token } = pageProps
+
+  const content = (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  )
+
   return (
     <>
-      <style jsx global>
-        {`
-          :root {
-            --font-family-sans: ${sans.style.fontFamily};
-            --font-family-serif: ${serif.style.fontFamily};
-            --font-family-mono: ${mono.style.fontFamily};
-          }
-        `}
-      </style>
       {draftMode ? (
-        <PreviewProvider token={token}>
-          <Component {...pageProps} />
-        </PreviewProvider>
+        <PreviewProvider token={token}>{content}</PreviewProvider>
       ) : (
-        <Component {...pageProps} />
+        content
       )}
     </>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,28 @@
-import type { GetStaticProps, InferGetStaticPropsType } from 'next'
-import { useLiveQuery } from 'next-sanity/preview'
+import { useQuery } from '@tanstack/react-query'
+import { request } from 'graphql-request'
 
 import Card from '~/components/Card'
 import Container from '~/components/Container'
 import Welcome from '~/components/Welcome'
-import { readToken } from '~/lib/sanity.api'
-import { getClient } from '~/lib/sanity.client'
-import { getPosts, type Post, postsQuery } from '~/lib/sanity.queries'
-import type { SharedPageProps } from '~/pages/_app'
+import { AllPostsQuery } from '~/lib/sanity.queries'
 
-export const getStaticProps: GetStaticProps<
-  SharedPageProps & {
-    posts: Post[]
-  }
-> = async ({ draftMode = false }) => {
-  const client = getClient(draftMode ? { token: readToken } : undefined)
-  const posts = await getPosts(client)
-
-  return {
-    props: {
-      draftMode,
-      token: draftMode ? readToken : '',
-      posts,
+export default function Home() {
+  const { data } = useQuery({
+    queryKey: 'allPosts',
+    queryFn: async () => {
+      const response = await request(
+        process.env.GRAPHQL_ENDPOINT,
+        AllPostsQuery,
+      )
+      return response.allPost
     },
-  }
-}
+  })
 
-export default function IndexPage(
-  props: InferGetStaticPropsType<typeof getStaticProps>,
-) {
-  const [posts] = useLiveQuery<Post[]>(props.posts, postsQuery)
   return (
     <Container>
-      <section>
-        {posts.length ? (
-          posts.map((post) => <Card key={post._id} post={post} />)
-        ) : (
-          <Welcome />
-        )}
-      </section>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+        {data?.map((post) => <Card key={post._id} post={post} />)}
+      </div>
     </Container>
   )
 }
