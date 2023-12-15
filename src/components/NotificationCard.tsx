9b82ea268032dee97a2b17ff465201d6bd54ca87
helenaskants <helenaskants@hotmail.com>
2023-12-15 14:36:46 +0100

    feat: add service cards + header (#6)

--- src/components/NotificationCard.tsx
+++ src/components/NotificationCard.tsx
@@ -4,27 +4,22 @@ import { Card, CardHeader, CardTitle } from '@/components/ui/card'
 
 const initialNotifications = [
   {
-    title: '1 Se åpningstider i julen',
+    title: 'Se åpningstider i julen',
     description:
       'Klikk her for å se åpningstider i perioden 22.desember - 29. desember.',
   },
   {
-    title: '2 Se åpningstider i julen',
+    title: 'Se åpningstider i julen',
     description:
       'Klikk her for å se åpningstider i perioden 22.desember - 29. desember.',
   },
   {
-    title: '3 Se åpningstider i julen',
+    title: 'Se åpningstider i julen',
     description:
       'Klikk her for å se åpningstider i perioden 22.desember - 29. desember.',
   },
   {
-    title: '4 Se åpningstider i julen',
-    description:
-      'Klikk her for å se åpningstider i perioden 22.desember - 29. desember.',
-  },
-  {
-    title: '4 Se åpningstider i julen',
+    title: 'Se åpningstider i julen',
     description:
       'Klikk her for å se åpningstider i perioden 22.desember - 29. desember.',
   },
@@ -33,7 +28,7 @@ const initialNotifications = [
 export default function NotificationSlideshow() {
   const slideRef = useRef(null)
   const totalNotifications = initialNotifications.length
-  const visibleSlides = 3 // Number of slides visible at a time
+  const visibleSlides = 2 // Number of slides visible at a time
   const duplicatedNotifications = [
     ...initialNotifications,
     ...initialNotifications,
@@ -57,11 +52,11 @@ export default function NotificationSlideshow() {
             style={{ width: `${100 / visibleSlides}%` }}
           >
             <Card className="bg-white bg-opacity-50 rounded-lg mx-2">
-              <CardHeader className="flex flex-row items-center p-2 mt-2 ml-2">
+              <CardHeader className="flex flex-row justify-between items-center p-2 mt-2 ml-2">
                 <CardTitle className="text-md text-xl">
                   {notification.title}
                 </CardTitle>
-                <img className="h-6 ml-28" src={'/bell-red.svg'} alt={'bell'} />
+                <img className="h-6 mr-4" src={'/bell-red.svg'} alt={'bell'} />
               </CardHeader>
               <div className="p-4">
                 <p>{notification.description}</p>
--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,3 +1,40 @@
+import React from 'react'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+
+const services = [
+  'Akupunktur',
+  'Psykolog',
+  'Helsestjenester for bedrifter',
+  'Reisemedisin og vaksiner',
+  'Dykkemedisin',
+  'Idrettsmedisin',
+  'Fysioterapi',
+  'Petroleums- og sjømannslege',
+]
+
 export default function Services() {
-  return <div className="welcome__container h-screen bg-white">Services</div>
+  return (
+    <div className="services-layout h-screen bg-white flex flex-col p-4">
+      <div className="header mb-10 pt-20 px-44 text-primary font-medium">
+        <h1 className="text-2xl">
+          I tillegg til vår fastlegekonsultasjon kan vi også hjelpe{' '}
+        </h1>
+        <h1 className="text-2xl">med en rekke andre tjenester</h1>
+      </div>
+      <div className=" mt-16 cards-container grid grid-cols-2 gap-4 mx-auto w-7/12">
+        {services.map((service) => (
+          <Card
+            key={service}
+            className="bg-white text-card-foreground shadow-lg rounded-lg border"
+          >
+            <CardContent className="flex justify-center items-center p-6">
+              <CardTitle className="text-2xl font-base leading-none tracking-tight">
+                {service}
+              </CardTitle>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+    </div>
+  )
 }
