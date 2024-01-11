890c88113dbbd8b38e154a202402a09be8df2337
helenaskants <helenaskants@hotmail.com>
2024-01-11 13:33:31 +0100

    fix height on card (#17)

--- src/components/NotificationCard.tsx
+++ src/components/NotificationCard.tsx
@@ -51,7 +51,7 @@ export default function NotificationSlideshow({
                 </CardTitle>
                 <img className="h-6 mr-4" src={'/bell-red.svg'} alt={'bell'} />
               </CardHeader>
-              <div className="p-4 text-lg prose">
+              <div className="p-4 text-lg prose max-h-[70px] overflow-hidden">
                 <PortableText value={notification.previewRaw} />
               </div>
             </Card>
